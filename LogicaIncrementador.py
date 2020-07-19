a3cf08e2e0e7a5b76837a402fe2fceefae3fd38f
Vladimir4063 <vladimirgutierrez4063@gmail.com>
2020-07-19 02:57:36 -0300

    Incorporamos el metodo While

--- LogicaIncrementador.py
+++ LogicaIncrementador.py
@@ -1,17 +1,24 @@
 
-num = int(input("Ingrese numero: "))
+lista = []
+def Incrementar(opc):
+    num = 1
+    while num != 0:
+        num = int(input("Ingrese numero: "))
+        if opc == 1:
+            resul = num * 1.21
+            lista.append(resul)
+        if opc == 2:
+            resul = num * 1.3
+    return lista
+    print("Resultado " + str(Incrementar(num, opc)))
+
+
 text = """ 
     1 - IVA
     2 - Ganancia
-"""
+    """
 print(text)
+
 opc = int(input("Ingrese opc: "))
-
-def Incrementar(num, opc):
-    if opc == 1:
-        resul = num * 1.21
-    if opc == 2:
-        resul = num * 1.3
-    return resul
-
-print("Resultado " + str(Incrementar(num, opc)))
+#num = int(input("Ingrese numero: "))
+print(Incrementar(opc))
